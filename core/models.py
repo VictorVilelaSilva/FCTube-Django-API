d95269dc77e60d60760415c9c1bcfd421aefb650
VictorVilelaSilva <victorsouzavilelasilva@gmail.com>
2024-11-18 01:28:20 -0300

    feat: finalizando parte inicial da api e consertando alguns erros

--- core/models.py
+++ core/models.py
@@ -1,8 +1,18 @@
+import hashlib
 from math import e
+import time
 from django.apps import AppConfig
 from django.db import models
 from django.forms import ValidationError
 from django.utils import timezone
+
+def random_filename(instance, filename):
+    # Extrai a extensão do arquivo
+    ext = filename.split('.')[-1]
+    
+    # Usa o timestamp atual e o nome original para gerar o hash simples
+    hash_object = hashlib.md5(f"{filename}{time.time()}".encode('utf-8'))
+    return f"{hash_object.hexdigest()}.{ext}"
 
 # Create your models here.
 # verbose_name é o nome que será exibido no admin
@@ -19,18 +29,29 @@ class Video(models.Model):
     author       = models.ForeignKey('auth.User',on_delete=models.CASCADE,verbose_name='Autor',editable=False)
 
     # A classe abaixo faz com que o nome da classe seja exibido no plural e singular no admin
-    class Meta:
-        verbose_name = 'Vídeo' #singular
-        verbose_name_plural = 'Vídeos'#plural
-    
-    def save(self, *args, **kwargs):
-        if self.is_published and not self.published_at:
-            self.published_at = timezone.now()
-        return super().save(*args, **kwargs)
+    def save(
+           self,
+           force_insert=False,
+           force_update=False,
+           using=None,
+           update_fields=None,
+       ):
+           if self.is_published and not self.published_at:
+               self.published_at = timezone.now()
+           return super().save(force_insert, force_update, using, update_fields)
 
-    def clean (self):
-        if self.is_published and not self.thumbnail and not self.video:
-            raise ValidationError('Para publicar um Vídeo é necessário enviar um thumbnail e um vídeo')
+    def clean(self):
+        if self.is_published:
+            if not hasattr(self, 'video_media'):
+                raise ValidationError('O vídeo não possui mídia associada.')
+            if self.video_media.status != VideoMedia.Status.PROCESS_FINISHED:
+                raise ValidationError('O vídeo não foi processado.')
+
+
+    def get_video_status_display(self):
+        if not hasattr(self, 'video_media'):
+            return 'Pendente'
+        return self.video_media.get_status_display()
         
 
     # A classe abaixo faz com que o nome do objeto seja exibido no admin
@@ -46,10 +67,13 @@ class VideoMedia(models.Model):
         PROCESS_FINISHED = 'PROCESSING_FINISHED', 'Processamento finalizado'
         PROCESS_ERROR = 'PROCESSING_ERROR', 'Erro no Processamento'
     
-    video_path = models.CharField(max_length=255, verbose_name='Videos')
+    video_path = models.CharField(max_length=255, verbose_name='Vídeo')
     status = models.CharField(max_length=20, choices=Status.choices, default=Status.UPLOADED_STARTED, verbose_name='Status')
-    video = models.OneToOneField(Video, on_delete=models.CASCADE, verbose_name='Vídeo', related_name='video_media')
+    video = models.OneToOneField(Video, on_delete=models.PROTECT, verbose_name='Vídeo', related_name='video_media')
 
+    def get_status_display(self):
+        return VideoMedia.Status(self.status).label
+    
     class Media:
         verbose_name = 'Media'
         verbose_name_plural = 'Midias'
